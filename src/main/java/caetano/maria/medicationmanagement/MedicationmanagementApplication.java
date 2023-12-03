fefe4a70350697075659a08e4ae6f17db8cfd938
Maria Vicente <mnvcaetano@gmail.com>
2023-12-03 17:26:16 -0300

    cria projeto spring

---
+++ src/main/java/caetano/maria/medicationmanagement/MedicationmanagementApplication.java
@@ -0,0 +1,13 @@
+package caetano.maria.medicationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicationmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicationmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/caetano/maria/medicationmanagement/MedicationmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package caetano.maria.medicationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
