af19d530f22d38a21b831cce157ffa5dc7e62fdf
Maria Vicente <mnvcaetano@gmail.com>
2023-12-03 17:30:01 -0300

    refactor: corrige o nome das classes

--- src/test/java/caetano/maria/medicationmanagement/MedicationManagementApplicationTests.java
+++ src/test/java/caetano/maria/medicationmanagement/MedicationManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MedicationmanagementApplicationTests {
+class MedicationManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
