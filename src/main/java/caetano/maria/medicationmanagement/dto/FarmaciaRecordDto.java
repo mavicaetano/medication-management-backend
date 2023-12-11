0c7c9e2cc64727e5494633db95cb690aef2d24af
Maria Vicente <mnvcaetano@gmail.com>
2023-12-11 20:47:20 -0300

    define os atributos da classe e validações

--- src/main/java/caetano/maria/medicationmanagement/dto/FarmaciaRecordDto.java
+++ src/main/java/caetano/maria/medicationmanagement/dto/FarmaciaRecordDto.java
@@ -1,2 +1,38 @@
-package caetano.maria.medicationmanagement.dto;public record FarmaciaRecordDto() {
+package caetano.maria.medicationmanagement.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class FarmaciaRecordDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @NotNull(message = "CNPJ não pode ser nulo.")
+    private Long cnpj;
+
+    @NotBlank(message = "Razão Social não pode estar em branco.")
+    private String razaoSocial;
+
+    @NotBlank(message = "Nome Fantasia não pode estar em branco.")
+    private String nomeFantasia;
+
+    @NotBlank(message = "E-mail não pode estar em branco.")
+    private String email;
+
+    private String telefone;
+
+    @NotNull(message = "Celular não pode ser nulo.")
+    private String celular;
+
+    @Valid
+    private EnderecoRecordDto endereco;
 }
