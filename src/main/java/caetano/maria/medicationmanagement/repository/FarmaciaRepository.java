f29535a54612eb8faa69be12463304601ed597ff
Maria Vicente <mnvcaetano@gmail.com>
2023-12-11 20:56:11 -0300

    cria método para validação de cnpj

--- src/main/java/caetano/maria/medicationmanagement/repository/FarmaciaRepository.java
+++ src/main/java/caetano/maria/medicationmanagement/repository/FarmaciaRepository.java
@@ -1,2 +1,13 @@
-package caetano.maria.medicationmanagement.repository;public interface FarmaciaRepository {
+package caetano.maria.medicationmanagement.repository;
+
+import caetano.maria.medicationmanagement.model.Farmacia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FarmaciaRepository extends JpaRepository<Farmacia, Long> {
+
+    @Query(value = "SELECT exists(SELECT 1 FROM farmacia WHERE cnpj = ?1)", nativeQuery = true)
+    boolean isCnpjExistente(Long cnpj);
 }
