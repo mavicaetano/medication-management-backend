53fee083a085e3da4f395b646c3d069eb5c84f00
Maria Vicente <mnvcaetano@gmail.com>
2023-12-11 20:35:39 -0300

    cria endpoints e as requisições

--- src/main/java/caetano/maria/medicationmanagement/controller/EstoqueController.java
+++ src/main/java/caetano/maria/medicationmanagement/controller/EstoqueController.java
@@ -1,2 +1,40 @@
-package caetano.maria.medicationmanagement.controller;public class EstoqueController {
+package caetano.maria.medicationmanagement.controller;
+
+import caetano.maria.medicationmanagement.dto.EstoqueAddMedicamentoDto;
+import caetano.maria.medicationmanagement.dto.EstoqueListagemDto;
+import caetano.maria.medicationmanagement.model.Estoque;
+import caetano.maria.medicationmanagement.service.EstoqueService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estoque")
+public class EstoqueController {
+    private final EstoqueService estoqueService;
+
+    public EstoqueController(EstoqueService estoqueService) {
+        this.estoqueService = estoqueService;
+    }
+
+    @GetMapping("/{cnpj}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<EstoqueListagemDto>> getAllEstoque(@PathVariable Long cnpj){
+        return ResponseEntity.status(HttpStatus.OK).body(estoqueService.findEstoquesByCnpj(cnpj));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Object> EstoqueAddMedicamentoDto(@RequestBody @Valid EstoqueAddMedicamentoDto estoqueAddMedicamentoDto) {
+        return ResponseEntity.status((HttpStatus.OK)).body(estoqueService.addMedicamengit add toNoEstoque(estoqueAddMedicamentoDto));
+    }
 }
