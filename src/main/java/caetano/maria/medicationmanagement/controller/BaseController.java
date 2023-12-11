7ca32da66b6362087b2362c881dc30b83cbf32fd
Maria Vicente <mnvcaetano@gmail.com>
2023-12-11 20:33:39 -0300

    cria endpoint de inicialização

--- src/main/java/caetano/maria/medicationmanagement/controller/BaseController.java
+++ src/main/java/caetano/maria/medicationmanagement/controller/BaseController.java
@@ -1,2 +1,31 @@
-package caetano.maria.medicationmanagement.controller;public class BaseController {
+package caetano.maria.medicationmanagement.controller;
+
+import caetano.maria.medicationmanagement.service.BaseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class BaseController {
+
+    private final BaseService baseService;
+
+    public BaseController(BaseService baseService) {
+        this.baseService = baseService;
+    }
+
+    @GetMapping
+    public String healthCheck() {
+        return "OK";
+    }
+
+    @PostMapping("/inicializacao")
+    @ResponseStatus(HttpStatus.OK)
+    public void inicializarDados() {
+        baseService.inicializarDados();
+    }
 }
