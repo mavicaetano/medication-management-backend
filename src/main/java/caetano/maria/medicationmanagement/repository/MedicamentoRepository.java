88eb5acae8ee03d30b709e4c47f74ac5abbb5159
Maria Vicente <mnvcaetano@gmail.com>
2023-12-11 20:56:48 -0300

    cria método para validação de numero de registro

--- src/main/java/caetano/maria/medicationmanagement/repository/MedicamentoRepository.java
+++ src/main/java/caetano/maria/medicationmanagement/repository/MedicamentoRepository.java
@@ -1,9 +1,13 @@
 package caetano.maria.medicationmanagement.repository;
 
-import caetano.maria.medicationmanagement.model.Farmacia;
+import caetano.maria.medicationmanagement.model.Medicamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FarmaciaRepository extends JpaRepository<Farmacia, Long> {
+public interface MedicamentoRepository extends JpaRepository<Medicamento, Integer> {
+
+    @Query(value = "SELECT exists(SELECT 1 FROM medicamento WHERE nro_registro = ?1)", nativeQuery = true)
+    boolean isNroRegistroExistente(Integer nroRegistro);
 }
