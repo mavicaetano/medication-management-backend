c573e8e945233451223da71936cfed7c155957e4
Maria Vicente <mnvcaetano@gmail.com>
2023-12-11 20:55:36 -0300

    cria métodos para busca de medicamento no estoque

--- src/main/java/caetano/maria/medicationmanagement/repository/EstoqueRepository.java
+++ src/main/java/caetano/maria/medicationmanagement/repository/EstoqueRepository.java
@@ -1,9 +1,17 @@
 package caetano.maria.medicationmanagement.repository;
 
-import caetano.maria.medicationmanagement.model.Medicamento;
+import caetano.maria.medicationmanagement.model.Estoque;
+import caetano.maria.medicationmanagement.model.IdEstoque;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {
+public interface EstoqueRepository extends JpaRepository<Estoque, IdEstoque> {
+
+    List<Estoque> findEstoquesByCnpj(Long cnpj);
+    Optional<Estoque> findEstoqueByCnpjAndNroRegistro(Long cnpj, Integer nroRegistro);
 }
