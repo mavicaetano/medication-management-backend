99e77570c168200c0b438d319725504021926dd3
Maria Vicente <mnvcaetano@gmail.com>
2023-12-11 20:46:40 -0300

    define os atributos da classe, adiciona getter e setter

--- src/main/java/caetano/maria/medicationmanagement/dto/EstoqueListagemDto.java
+++ src/main/java/caetano/maria/medicationmanagement/dto/EstoqueListagemDto.java
@@ -1,2 +1,21 @@
-package caetano.maria.medicationmanagement.dto;public class EstoqueListagemDto {
+package caetano.maria.medicationmanagement.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class EstoqueListagemDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer nroRegistro;
+    private String nome;
+    private Integer quantidade;
+    private LocalDateTime dataAtualizacao;
 }
