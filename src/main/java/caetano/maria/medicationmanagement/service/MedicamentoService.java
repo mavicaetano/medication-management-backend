587826e227406d210ae88b3f4948448b1fe18889
Maria Vicente <mnvcaetano@gmail.com>
2023-12-11 21:00:02 -0300

    cria método para criação de medicamentos

--- src/main/java/caetano/maria/medicationmanagement/service/MedicamentoService.java
+++ src/main/java/caetano/maria/medicationmanagement/service/MedicamentoService.java
@@ -1,2 +1,38 @@
-package caetano.maria.medicationmanagement.service;public class MedicamentoService {
+package caetano.maria.medicationmanagement.service;
+
+import caetano.maria.medicationmanagement.dto.MedicamentoDto;
+import caetano.maria.medicationmanagement.model.Medicamento;
+import caetano.maria.medicationmanagement.repository.MedicamentoRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MedicamentoService {
+
+    private final MedicamentoRepository medicamentoRepository;
+
+    public MedicamentoService(MedicamentoRepository medicamentoRepository) {
+        this.medicamentoRepository = medicamentoRepository;
+    }
+    public List<Medicamento> findAll() {
+        return medicamentoRepository.findAll();
+    }
+
+    public Optional<Medicamento> findById(Integer nroRegistro) {
+        return medicamentoRepository.findById(nroRegistro);
+    }
+
+    public ResponseEntity<Object> saveMedicamento(MedicamentoDto medicamentoDto) {
+        if (medicamentoRepository.isNroRegistroExistente(medicamentoDto.getNroRegistro())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Número de Registro já existe.");
+        }
+        var medicamento = new Medicamento();
+        BeanUtils.copyProperties(medicamentoDto, medicamento);
+        return ResponseEntity.status(HttpStatus.CREATED).body(medicamentoRepository.save(medicamento));
+    }
 }
